b30476434b26591366ff3c0cb641d8106c40c80e
IrajBuchicago <iraj@uchicago.edu>
2026-02-01 15:57:33 -0600

    Update data sources

--- data_sources/#311 Service Requests data sources.py
+++ data_sources/#311 Service Requests data sources.py
@@ -6,3 +6,25 @@
 
 https://data.cityofchicago.org/api/v3/views/v6vf-nfxy/query.json
 
+
+#Chicago Neighborhoods zip file - Chicago Data Portal
+
+#Link to API endpoint:
+
+https://data.cityofchicago.org/api/v3/views/y6yq-dbs2/query.json
+
+#Zip codes
+
+#Link:
+
+https://data.cityofchicago.org/api/v3/views/unjd-c2ca/query.json
+
+#Tracts 2020
+
+#Link:
+
+https://data.cityofchicago.org/api/v3/views/74p9-q2aq/query.json
+
+
+
+
